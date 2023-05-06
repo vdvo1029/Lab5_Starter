e16a2f5d9766346b4429331373454e0b95f4f013
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2023-05-05 17:55:17 -0700

    image changes with value selection

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,17 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const element = document.getElementById('horn-select');
+  element.addEventListener("change", (event) => {
+    var img = document.querySelector('img');
+    if (event.target.value == 'air-horn') {
+      img.src="assets\\images\\air-horn.svg";
+    } else if (event.target.value == 'car-horn') {
+      img.src="assets\\images\\car-horn.svg";
+    } else if (event.target.value == 'party-horn') {
+      img.src= "assets\\images\\party-horn.svg";
+    } else {
+      img.src= "assets\\images\\no-image.png";
+    }
+  });
 }
