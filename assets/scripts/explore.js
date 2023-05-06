339583a56f4d9ee180412af4fbc059e185322bf1
vdvo1029 <97643587+vdvo1029@users.noreply.github.com>
2023-05-05 19:17:48 -0700

    Merge branch 'master' of https://github.com/vdvo1029/Lab5_Starter

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,47 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+    let speech = window.speechSynthesis;
+    let voices = [];
+    
+    
+
+    let voiceSelect = document.getElementById("voice-select");
+    console.log("hello");
+    
+    function populateVoiceList(){
+        voices = speech.getVoices();
+        for (let i = 0; i < voices.length; i++) {
+            const option = document.createElement("option");
+            option.textContent = `${voices[i].name} (${voices[i].lang})`;
+        
+            option.setAttribute("data-name", voices[i].name);
+            voiceSelect.appendChild(option);
+        }
+    }
+    populateVoiceList();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+      }
+      const button = document.querySelector('button');
+    button.addEventListener("click", (event)=>{
+        let text = document.getElementById("text-to-speak");
+        
+
+
+        const utterThis = new SpeechSynthesisUtterance(text.value);
+        const selectedOption =
+          voiceSelect.selectedOptions[0].getAttribute("data-name");
+        for (let i = 0; i < voices.length; i++) {
+          if (voices[i].name === selectedOption) {
+            utterThis.voice = voices[i];
+          }
+        }
+        
+        speech.speak(utterThis);
+      
+
+        
+    });
 
 }
