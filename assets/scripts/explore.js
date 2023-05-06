37dc5aab072079c9238447a49a3ba0af8bc520c7
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2023-05-06 15:58:43 -0700

    updated explore pt1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,45 +5,44 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
     let speech = window.speechSynthesis;
     let voices = [];
-    
-    
 
     let voiceSelect = document.getElementById("voice-select");
-    console.log("hello");
     
+    // Some credit to the SpeechSynthesis docs
     function populateVoiceList(){
         voices = speech.getVoices();
         for (let i = 0; i < voices.length; i++) {
             const option = document.createElement("option");
-            option.textContent = `${voices[i].name} (${voices[i].lang})`;
-        
-            option.setAttribute("data-name", voices[i].name);
+            option.textContent = `${voices[i].name})`;
+            option.setAttribute("voice", voices[i].name);
             voiceSelect.appendChild(option);
         }
     }
+
     populateVoiceList();
+    
+    // SpeechSynthesis docs, to ensure that the site will load the voices if the voice options change
     if (speechSynthesis.onvoiceschanged !== undefined) {
         speechSynthesis.onvoiceschanged = populateVoiceList;
       }
-      const button = document.querySelector('button');
+    
+    const button = document.querySelector('button');
     button.addEventListener("click", (event)=>{
         let text = document.getElementById("text-to-speak");
-        
 
-
-        const utterThis = new SpeechSynthesisUtterance(text.value);
-        const selectedOption =
-          voiceSelect.selectedOptions[0].getAttribute("data-name");
-        for (let i = 0; i < voices.length; i++) {
-          if (voices[i].name === selectedOption) {
-            utterThis.voice = voices[i];
+        const talk = new SpeechSynthesisUtterance(text.value);
+        const selectedOption = voiceSelect.selectedOptions[0].getAttribute('voice');
+        if (selectedOption != null) {
+          for (let i = 0; i < voices.length; i++) {
+            if (voices[i].name === selectedOption) {
+              talk.voice = voices[i];
+            }
+          }
+          
+          if (!speech.speaking) {
+            speech.speak(talk);
           }
         }
-        
-        speech.speak(utterThis);
-      
-
-        
     });
 
 }
