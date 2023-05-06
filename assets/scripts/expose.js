c730313a3478a534a841f923a03d524028abce75
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2023-05-05 18:13:29 -0700

    Audio button mute, confetti, audio plays

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,8 +3,11 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const element = document.getElementById('horn-select');
-  element.addEventListener("change", (event) => {
+  const selector = document.getElementById('horn-select');
+  const confetti = new JSConfetti();
+
+  // Change image based on user selection
+  selector.addEventListener("change", (event) => {
     var img = document.querySelector('img');
     if (event.target.value == 'air-horn') {
       img.src="assets\\images\\air-horn.svg";
@@ -15,5 +18,44 @@ function init() {
     } else {
       img.src= "assets\\images\\no-image.png";
     }
+  });
+  
+  // Play audio on button press
+  const button = document.querySelector('button');
+  button.addEventListener("click", (event) => {
+    var audio = document.querySelector('audio')
+    if (selector.value == 'air-horn') {
+      audio.src="assets\\audio\\air-horn.mp3";
+    } else if (selector.value == 'car-horn') {
+      audio.src="assets\\audio\\car-horn.mp3";
+    } else if (selector.value == 'party-horn') {
+      audio.src="assets\\audio\\party-horn.mp3";
+    } else {
+      audio.src="";
+    }
+    audio.play();
+    if (selector.value == 'party-horn') {
+      confetti.addConfetti();
+    }
+  });
+
+  // Volume icon changes according to value on slider
+  let volume = document.getElementById("volume");
+  volume.addEventListener("click", (event) =>{
+    let volumeicon = document.querySelector("[alt='Volume level 2']");
+    volumeicon.src = "assets/icons/volume-level-3.svg";
+    if(volume.value < 67){
+      volumeicon.src = "assets/icons/volume-level-2.svg";
+    }
+    if(volume.value < 33){
+      volumeicon.src = "assets/icons/volume-level-1.svg";
+    }
+
+    if(volume.value==0){
+
+      volumeicon.src = "assets/icons/volume-level-0.svg";
+    }
+
+
   });
 }
