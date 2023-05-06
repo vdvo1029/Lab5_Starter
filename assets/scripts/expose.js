f509a6b9ff2ba5e2eebdfce343ea9961b0a9eb2b
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2023-05-05 18:28:04 -0700

    volume updates

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,7 +8,7 @@ function init() {
 
   // Change image based on user selection
   selector.addEventListener("change", (event) => {
-    var img = document.querySelector('img');
+    let img = document.querySelector('img');
     if (event.target.value == 'air-horn') {
       img.src="assets\\images\\air-horn.svg";
     } else if (event.target.value == 'car-horn') {
@@ -23,7 +23,7 @@ function init() {
   // Play audio on button press
   const button = document.querySelector('button');
   button.addEventListener("click", (event) => {
-    var audio = document.querySelector('audio')
+    let audio = document.querySelector('audio')
     if (selector.value == 'air-horn') {
       audio.src="assets\\audio\\air-horn.mp3";
     } else if (selector.value == 'car-horn') {
@@ -43,6 +43,8 @@ function init() {
   let volume = document.getElementById("volume");
   volume.addEventListener("click", (event) =>{
     let volumeicon = document.querySelector("[alt='Volume level 2']");
+    let audio = document.querySelector('audio');
+
     volumeicon.src = "assets/icons/volume-level-3.svg";
     if(volume.value < 67){
       volumeicon.src = "assets/icons/volume-level-2.svg";
@@ -56,6 +58,7 @@ function init() {
       volumeicon.src = "assets/icons/volume-level-0.svg";
     }
 
+    audio.volume = volume.value/100;
 
   });
 }
